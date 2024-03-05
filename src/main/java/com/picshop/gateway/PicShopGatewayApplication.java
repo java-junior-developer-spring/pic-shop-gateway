edd508445e1aa9c8f93d433dec04d3edb761b143
dasha <jjg-lessons@gmail.com>
2024-03-06 00:24:15 +0300

    Создание, настройка

---
+++ src/main/java/com/picshop/gateway/PicShopGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.picshop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicShopGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicShopGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/picshop/gateway/PicShopGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picshop.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicShopGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
